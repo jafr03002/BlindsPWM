8f5cee6b06e8175f230c1b438ae49ab6ae48ceb6
frja22sv <129272707+frja22sv@users.noreply.github.com>
2025-01-30 15:22:28 +0100

    implemented time event handler

--- main/main.c
+++ main/main.c
@@ -5,23 +5,77 @@
 #include "sdkconfig.h"
 #include "driver/ledc.h"
 #include "driver/gpio.h"
+#include "sys/time.h"
+#include <stdio.h>
+#include <string.h>
 
 /* BlindsPWM
-    PWM Blinds is an automatic blinds operator powered by a servo motor
-    The servo has 2 modes: ON or OFF, meaning the blinds are shut or not
-    These modes are dependent on the PWM signal from channel 0
-    When altered the servo motor will effectively switch the blinds.
-*/
+ *   PWM Blinds is an automatic blinds operator powered by a servo motor
+ *   The servo has 2 modes: ON or OFF, meaning the blinds are open or closed
+ *   These modes are dependent on the PWM signal from channel 0
+ *   When altered the servo motor will effectively switch the blinds.
+ */
 
 #define LED_PIN 18
+#define PWM_PIN 25
+#define OPEN_BLINDS_TIME "08:00:00"
+#define CLOSE_BLINDS_TIME "16:00:00"
+
+uint16_t duty = 819; // varies form 819-4096 (2.5%-12.5% of 2^15)
+uint16_t step = 7; //each step in duty increment
+uint16_t total_cycles = 467; //total steps
+bool pos_direction = true; //true = open, false = closed 
+uint8_t iteration_time = 10; //10 ms 
+char time_buf[9]; //HH:MM:SS
+
+
+ //gets the running real time and formats it
+void get_time(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    struct tm * timeinfo;
+    timeinfo = localtime(&tv.tv_sec);
+
+    sprintf(time_buf,"%02d:%02d:%02d",timeinfo->tm_hour
+                                     ,timeinfo->tm_min
+                                     ,timeinfo->tm_sec);  
+}
+
+/*
+ *    Waiting function polling and comparing the correct event times with real time.
+ *    then returns resulting in resuming the program
+ */
+void wait_for_event_time(void){
+    if(!pos_direction){ //Closed
+        while(1){
+            get_time();
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+            if(strcmp(time_buf,OPEN_BLINDS_TIME)==0){ //Open blinds time
+                return;
+            }
+        }
+    }
+    while(1){ //Open
+        get_time();
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        if(strcmp(time_buf,CLOSE_BLINDS_TIME)==0){ //Close blinds time
+            return;
+        }
+    }
+}
+
+/*
+ * Main function loop of the program
+ * configures ledc timer and ledc pwm channel
+ * if servo position is true it moves counter-clockwise
+ * incrementing the dutycycle changing its angle and vice versa
+ * then waits for an event time
+ * when event time has accured the servo position-direction changes
+ * repeat
+ */
 
 void servoRotate_task(void *args){
-
-    uint16_t duty = 819; // varies form 819-4096 (2.5%-12.5% of 2^15)
-    uint8_t step = 7;
-    uint16_t total_cycles = 467;
-    bool pos_direction = true; //up or down
-    uint8_t iteration_time = 10; //10 ms
 
     ledc_timer_config_t ledc_timer = {
         .speed_mode          = LEDC_HIGH_SPEED_MODE,
@@ -38,7 +92,7 @@ void servoRotate_task(void *args){
         .channel    = LEDC_CHANNEL_0,
         .timer_sel  = LEDC_TIMER_0,
         .intr_type  = LEDC_INTR_DISABLE,
-        .gpio_num   = 25,
+        .gpio_num   = PWM_PIN,
         .duty       = duty,
         .hpoint     = 0
     };
@@ -57,8 +111,11 @@ void servoRotate_task(void *args){
 
             vTaskDelay(iteration_time/portTICK_PERIOD_MS);
         }
+        wait_for_event_time();
         gpio_set_level(LED_PIN, 1);
+
         pos_direction = !pos_direction;
+
         vTaskDelay(1000/portTICK_PERIOD_MS);
         gpio_set_level(LED_PIN, 0);
     }
