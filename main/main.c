c4b8d432f75822a3502507c3705fa3a95ee8b7d4
frja22sv <129272707+frja22sv@users.noreply.github.com>
2025-03-05 11:37:58 +0100

    added debounce logic

--- main/main.c
+++ main/main.c
@@ -2,9 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/timers.h"
 #include "sdkconfig.h"
 #include "driver/ledc.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_timer.h"
 #include "sys/time.h"
 #include <stdio.h>
 #include <string.h>
@@ -22,6 +25,7 @@
 #define MAX_DUTY 
 #define OPEN_BLINDS_TIME "08:00:00"
 #define CLOSE_BLINDS_TIME "16:00:00"
+#define BOUNCE_DELAY_MS 20
 
 float duty = 1001; // varies from 1001(3%)-4221(12.8%)
 uint16_t step = 14; //each step in duty increment
@@ -29,14 +33,19 @@ uint16_t total_cycles = 230; //total steps
 bool pos_direction = true; //true = open, false = closed 
 uint8_t iteration_time = 10; //10 ms 
 char time_buf[9]; //HH:MM:SS
-bool btn_press = 0;
-int isr_tick;
-int now;
-int dbug_time;
+bool btn_press = false;
+
+static TimerHandle_t debounce_timer;
 
 static void IRAM_ATTR gpio_isr_handler(void* arg){
-    isr_tick = (int)xTaskGetTickCount;
-    btn_press = 1;
+    gpio_intr_disable(BTN_PIN);
+    xTimerStartFromISR(debounce_timer, NULL); 
+}
+void debounce_timer_callback(TimerHandle_t xTimer){
+    if(gpio_get_level(BTN_PIN)==1){
+        btn_press = true;
+    }
+    gpio_intr_enable(BTN_PIN);
 }
 
  //gets the running real time and formats it
@@ -62,8 +71,8 @@ void wait_for_event(void){
             get_time();
             vTaskDelay(1000/portTICK_PERIOD_MS);
             if(strcmp(time_buf,OPEN_BLINDS_TIME)==0 || btn_press){ //Open blinds time 08:00:00
-                btn_press = 0;
-                return;
+                btn_press = false;
+                return;   
             }
         }
     }
@@ -72,8 +81,8 @@ void wait_for_event(void){
         vTaskDelay(1000/portTICK_PERIOD_MS);
         if(strcmp(time_buf,CLOSE_BLINDS_TIME)==0 || btn_press){ //Close blinds time 16:00:00
             btn_press = 0;
-            return;
-        }
+            return;    
+        }                  
     }
 }
 
@@ -123,10 +132,11 @@ void servoRotate_task(void *args){
     gpio_isr_handler_add(BTN_PIN, gpio_isr_handler, NULL);
     gpio_intr_enable(BTN_PIN);
 
+    debounce_timer = xTimerCreate("DebounceTimer", (BOUNCE_DELAY_MS)/portTICK_PERIOD_MS, pdFALSE, NULL, debounce_timer_callback);
+
     int i;
     while(1) {
         for (i=0; i < total_cycles; i++){
-            
             pos_direction ? (duty += step) : (duty -= step);
             ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty);
             ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
